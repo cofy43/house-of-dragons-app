4e05fce50e6d52362373d04de789e1f3730d2bcf
Martin Felipe Espinal Cruces <cofy43@gmail.com>
2023-01-06 14:59:25 -0600

    change theme implemented

--- lib/home.dart
+++ lib/home.dart
@@ -2,29 +2,56 @@ import 'package:flutter/material.dart';
 import 'cards_list.dart';
 
 class Home extends StatefulWidget {
+  const Home({super.key});
+
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _Home();
   }
   
 }
 
 class _Home extends State<Home> {
+
+  static final ValueNotifier<ThemeMode> themeNotifier = ValueNotifier(ThemeMode.light);
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false, 
-      home: Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: const Text("Games of thrones"),
-        ),
-        body: CardList(),
-      ),
+
+    return ValueListenableBuilder<ThemeMode>(
+      valueListenable: themeNotifier,
+        builder: (_, ThemeMode currentMode, __) {
+          return MaterialApp(
+            title: 'Games of thrones app',
+            theme: ThemeData.light(),
+            darkTheme: ThemeData.dark(),
+            themeMode: currentMode,
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              appBar: AppBar(
+                centerTitle: true,
+                title: const Text("Games of thrones"),
+                actions: [
+                  IconButton(
+                      padding: const EdgeInsets.only(right: 30, top: 2),
+                      icon: Icon(
+                          _Home.themeNotifier.value == ThemeMode.light
+                            ? Icons.ac_unit
+                            : Icons.local_fire_department,
+                          size: 40,
+                      ),
+                      onPressed: () {
+                        _Home.themeNotifier.value =
+                        _Home.themeNotifier.value == ThemeMode.light
+                            ? ThemeMode.dark
+                            : ThemeMode.light;
+                      },
+                  )
+                ],
+              ),
+              body: CardList(),
+            ),
+          );
+        }
     );
   }
   
